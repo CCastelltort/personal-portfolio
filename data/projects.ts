dd9f8862528aa4bc0fefcad1ba427957dcd64abe
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-08-10 01:07:10 +0200

    Only show repo button if there is a github repo

--- data/projects.ts
+++ data/projects.ts
@@ -21,14 +21,14 @@ const projects: Project[] = [
     github: "https://github.com/CCastelltort/ROBO-CON-2025",
   },
   {
-    title: "Recipe Management System",
+    title: "AI-Integrated Jacket Design Prototype",
     description: [
-      "Designed and populated a relational database using MySQL Workbench by creating an ER diagram, forward-engineering it into a normalized schema, and populating 9 tables with 500+ data entries",
-      "Built a Python-based Command-Line Interface (CLI) integrating 8 optimized SQL queries for efficient data retrieval and modification, with cascading updates and deletes to maintain relational integrity",
+      "Developed a prototype website for Revelia, an early-stage startup focused on AI-driven fashion customization. The prototype was created specifically for a startup competition, serving as a live demo to pitch Revelia’s business model and product vision",
+      "Built using Next.js with the App Router, TypeScript, and Tailwind CSS with a focus on clean, modular architecture where each page was broken into reusable components for clarity and scalability",
+      "The platform allows users to fully personalize their jackets, including colors, buttons, pockets, embroidery and more, or generate unique designs using an integrated AI model using natural language prompts"
     ],
-    image: "/recipeManagement.png",
-    tag: ["SQL", "MySQL", "Python"],
-    github: "https://github.com/CCastelltort/recipe-management-system",
+    image: "/revelia.png",
+    tag: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
   },
   {
     title: "FrogCrew - Sports Broadcasting Management Platform",
@@ -43,16 +43,14 @@ const projects: Project[] = [
     inProgress: true,
   },
   {
-    title: "AI-Integrated Jacket Design Prototype",
+    title: "Recipe Management System",
     description: [
-      "Developed a prototype website for Revelia, an early-stage startup focused on AI-driven fashion customization.The prototype was created specifically for a startup competition, serving as a live demo to pitch Revelia’s business model and product vision",
-      "Built using Next.js with the App Router, TypeScript, and Tailwind CSS with a focus on clean, modular architecture where each page was broken into reusable components for clarity and scalability",
-      "The platform allows users to fully personalize their jackets, including colors, buttons, pockets, embroidery and more, or generate unique designs using an integrated AI model using natural language prompts"
+      "Designed and populated a relational database using MySQL Workbench by creating an ER diagram, forward-engineering it into a normalized schema, and populating 9 tables with 500+ data entries",
+      "Built a Python-based Command-Line Interface (CLI) integrating 8 optimized SQL queries for efficient data retrieval and modification, with cascading updates and deletes to maintain relational integrity",
     ],
-    image: "/revelia.png",
-    tag: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
-    github: "",
-    demo: "",
+    image: "/recipeManagement.png",
+    tag: ["SQL", "MySQL", "Python"],
+    github: "https://github.com/CCastelltort/recipe-management-system",
   },
   {
     title: "Project 3",
